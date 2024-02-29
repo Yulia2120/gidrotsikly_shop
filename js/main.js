6a884f2c1db701e567b9bf5a8b42b79e83c196e3
Yuliya <iksulia1@gmail.com>
2024-02-29 11:22:15 +0200

    fix tab main.js

--- js/main.js
+++ js/main.js
@@ -10,8 +10,10 @@ $(function () {
   $(".tab").on("click", function (e) {
     e.preventDefault();
 
-    $(".tab").removeClass("tab_active");
-    $(".tabs-content").removeClass("tabs-content_active");
+    $($(this).siblings()).removeClass("tab_active");
+    $($(this).parent().siblings().find("div")).removeClass(
+      "tabs-content_active"
+    );
 
     $(this).addClass("tab_active");
     $($(this).attr("href")).addClass("tabs-content_active");
