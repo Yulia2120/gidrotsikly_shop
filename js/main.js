7baee01e73fceaced75c6099a60ff91a34ce3df5
Yuliya <iksulia1@gmail.com>
2024-03-13 15:45:29 +0200

    range-slider

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@
 //   });
 
 $(function () {
+  // slick-index
   const mainSlider = $(".banner-section__slider");
 
   mainSlider.on("init", function (slick) {
@@ -25,6 +26,7 @@ $(function () {
       '<button class="banner-section__slider-btn banner-section__slider-btn_next"><img src="images/slider/right.svg" alt="arrow right" /></button>',
   });
 
+  //tabs
   $(".tab").on("click", function (e) {
     e.preventDefault();
 
@@ -41,6 +43,7 @@ $(function () {
     $(this).toggleClass("product-item__favorite_active");
   });
 
+  //slick-product
   $(function () {
     const productSlider = $(".product-slider");
 
@@ -59,14 +62,16 @@ $(function () {
       nextArrow:
         '<button class="product-slider__slider-btn product-slider__slider-btn_next"><img src="images/icons/arrow-next.svg" alt="arrow next" /></button>',
     });
-
-    // $(".product-slider").slick({
-    //   slidesToShow: 4,
-    //   slidesToScroll: 1,
-    //   prevArrow:
-    //     '<button class="product-slider__slider-btn product-slider__slider-btn_prev"><img src="images/icons/arrow-prev.svg" alt="arrow prev" /></button>',
-    //   nextArrow:
-    //     '<button class="product-slider__slider-btn product-slider__slider-btn_next"><img src="images/icons/arrow-next.svg" alt="arrow next" /></button>',
-    // });
   });
+
+  //jquery-form-styler
+  $(".filter-style").styler();
+
+  $(".filter__item-drop").on("click", function () {
+    $(this).toggleClass("filter__item-drop_active");
+    $(this).next().slideToggle(200);
+  });
+
+  //range-slider
+  $(".js-range-slider").ionRangeSlider();
 });
